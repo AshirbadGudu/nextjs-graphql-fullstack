b13f181a8c168bccd5efb8448e50d0a5f1029952
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2023-01-25 12:04:25 +0530

    in resolver try catch block added with other error handling

--- resolvers/index.ts
+++ resolvers/index.ts
@@ -5,41 +5,52 @@ const resolvers: Resolvers = {
   Query: { cart: (_, { id }) => ({ id, totalItems: 0 }) },
   Mutation: {
     sendEmail: async (_, { html, to }) => {
-      if (!html?.length)
-        return {
-          __typename: "FailureResponse",
-          error: {
-            message: `Need proper body to send email`,
-            code: "INVALID_EMAIL_BODY",
-          },
-        };
+      try {
+        if (!html?.length)
+          return {
+            __typename: "FailureResponse",
+            error: {
+              message: `Need proper body to send email`,
+              code: "INVALID_EMAIL_BODY",
+            },
+          };
 
-      if (!to?.length)
+        if (!to?.length)
+          return {
+            __typename: "FailureResponse",
+            error: {
+              message: `Provided email address is not a valid`,
+              code: "INVALID_RECIPIENT_EMAIL",
+            },
+          };
+        const { rejected, response } = await transporter.sendMail({
+          from: `${CONFIG.MAIL_ADDRESS}`,
+          to,
+          subject: "Subject: ",
+          html,
+        });
+        if (rejected?.length)
+          return {
+            __typename: "FailureResponse",
+            error: {
+              message: `Some of the recipients were rejected`,
+              code: "MAIL_REJECTED",
+            },
+          };
+        return {
+          __typename: "SuccessResponse",
+          message: `Mail sent to ${to}`,
+        };
+      } catch (error) {
+        console.log(error);
         return {
           __typename: "FailureResponse",
           error: {
-            message: `Provided email address is not a valid`,
-            code: "INVALID_RECIPIENT_EMAIL",
+            message: `Some of the recipients were rejected`,
+            code: "MAIL_REJECTED",
           },
         };
-      const { rejected, response } = await transporter.sendMail({
-        from: `${CONFIG.MAIL_ADDRESS}`,
-        to,
-        subject: "Subject: ",
-        html,
-      });
-      if (rejected?.length)
-        return {
-          __typename: "FailureResponse",
-          error: {
-            message: `Mail error: ${response}`,
-            code: "INTERNAL_SERVER_ERROR",
-          },
-        };
-      return {
-        __typename: "SuccessResponse",
-        message: `Mail sent to ${to}`,
-      };
+      }
     },
   },
 };
