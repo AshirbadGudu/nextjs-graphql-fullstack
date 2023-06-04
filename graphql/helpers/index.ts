131e75d2bb98c6288041733e93a2fe30d46506cd
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2023-06-04 11:36:35 +0530

    read gql separate function created

--- graphql/helpers/index.ts
+++ graphql/helpers/index.ts
@@ -1,3 +1,5 @@
+import { readFileSync } from "fs";
+import { join } from "path";
 // Helper function to generate a new unique ID (you can implement your own logic)
 export function generateNewId() {
   return `${Date.now()}`;
@@ -6,4 +8,10 @@ export function generateNewId() {
 // Helper function to get the current date and time
 export function getCurrentDateTime() {
   return new Date().toISOString();
+}
+
+export function readGraphql({ schema }: { schema: String }) {
+  return readFileSync(join(process.cwd(), `graphql/schemas/${schema}.gql`), {
+    encoding: "utf-8",
+  });
 }
